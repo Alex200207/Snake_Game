ef4f1f5d3b208b2381359b4fcf0bd2d832374c28
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-08 16:29:15 -0600

    fix: correct food positioning in game initialization logic

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let foodX = 13,
   foodY = 10;
 const initGame = () => {
   let htmlMarkup = `<div class="food style="grid-area:${foodY} / ${foodX}"></div>`;
+  playBoard.innerHTML = htmlMarkup;
 };
 
 initGame();
