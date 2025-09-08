1ccb950b8d2af867abc2481b6b38ad5f73b68896
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-08 16:46:05 -0600

    refactor: reorganize snake movement and velocity logic in script.js

--- script.js
+++ script.js
@@ -8,12 +8,28 @@ let foodX = 13, foodY = 10;
 //cordenas de la serpiente
 let snakeX = 5, snakeY = 10;
 
+//velocidad de la serpiente
+let velocityX = 0, velocityY = 0;
+
   const changeFoodPosition = () => {
     //buscar al asar un numero entre 1 y 30
     foodX = Math.floor(Math.random() * 30) + 1
     foodY = Math.floor(Math.random() * 30) + 1
     console.log(foodX)
   }
+
+//funcion para mover la serpiente
+//en la web las cordenadas haciar arriba son negativas y hacia abajo positivas
+const changeDirection = (e) => {
+   if(e.key === "ArrowUp"){
+    //se le pone 0 para que no se mueva en x
+    velocityX = 0;
+    //se resta 1 para que vaya hacia arriba
+    velocityY = -1;
+   }
+}
+
+
 const initGame = () => {
   let htmlMarkup = `<div class="food" style="grid-area:${foodY} / ${foodX}"></div>`;
   
@@ -27,3 +43,7 @@ const initGame = () => {
 //lamado a la funcion
 changeFoodPosition()
 initGame();
+
+//nuevo evento
+
+document.addEventListener('keydown', changeDirection);
