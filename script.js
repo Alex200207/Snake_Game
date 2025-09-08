4c1a8601a705229623975a65db9ffa0444313897
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-08 16:34:22 -0600

    fix: correct food positioning in HTML markup and improve food position logic

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 //funcion inicializadora del juego
 const playBoard = document.querySelector(".play-board");
 
-let foodX = 13,
-  foodY = 10;
+
+//cordenas de la comida
+let foodX = 13, foodY = 10;
+
+  const changeFoodPosition = () => {
+    //buscar al asar un numero entre 1 y 30
+    foodX = Math.floor(Math.random() * 30) + 1
+    foodY = Math.floor(Math.random() * 30) + 1
+    console.log(foodX)
+  }
 const initGame = () => {
-  let htmlMarkup = `<div class="food style="grid-area:${foodY} / ${foodX}"></div>`;
+  let htmlMarkup = `<div class="food" style="grid-area:${foodY} / ${foodX}"></div>`;
   playBoard.innerHTML = htmlMarkup;
 };
 
+//lamado a la funcion
+changeFoodPosition()
 initGame();
