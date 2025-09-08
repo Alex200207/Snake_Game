eefe6d76d6102ec40920fbaac26e25f9991703a8
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-09-08 16:40:36 -0600

    feat: add snake creation logic and styling to the game

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const playBoard = document.querySelector(".play-board");
 //cordenas de la comida
 let foodX = 13, foodY = 10;
 
+//cordenas de la serpiente
+let snakeX = 5, snakeY = 10;
+
   const changeFoodPosition = () => {
     //buscar al asar un numero entre 1 y 30
     foodX = Math.floor(Math.random() * 30) + 1
@@ -13,6 +16,11 @@ let foodX = 13, foodY = 10;
   }
 const initGame = () => {
   let htmlMarkup = `<div class="food" style="grid-area:${foodY} / ${foodX}"></div>`;
+  
+  //crear serpiente
+
+  htmlMarkup += `<div class="snake" style="grid-area:${snakeY} / ${snakeX}"></div>`;
+
   playBoard.innerHTML = htmlMarkup;
 };
 
